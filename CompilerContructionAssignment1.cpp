fd70e595089315123a8875d12031100776e6ffe1
Zohaib khan <zk510873@gmail.com>
2025-04-26 23:17:47 -0600

    Compiler Construction Assignment Updated

--- CompilerContructionAssignment1.cpp
+++ CompilerContructionAssignment1.cpp
@@ -1,42 +1,69 @@
 /*
-Name : Zohaib Khan 
-class no. BSCS-221335 
-Section A 6th Semester 
-Instructor : Sir Zubair
-Subject : Computer Science
-Assignment #01 Compiler Construction
-ISLAMIA COLLEGE PESHAWER (CHARETERED UNIVERSITY)
+Name : Zohaib Khan  
+Class No. : BSCS-221335  
+Section : A (6th Semester)  
+Instructor : Sir Zubair  
+Subject : Compiler Construction  
+College : Islamia College Peshawar (Chartered University)
 */
+
 #include<iostream>
 #include<fstream>
 using namespace std;
-string checkWord(string word){
-	int len = word.length();
-	string msg;
-	if(len%2==0)
-		msg = "Invalid";
-	else
-		msg = "Valid";	
-	return msg;
+
+char buff[1000];
+int forward = 0;
+
+char next_char();
+char lex_anal();
+void read_from_file();
+
+int main(){
+    read_from_file();
+
+    char t = lex_anal();
+    cout <<"File Text Valid/Invalid [y/n] : "<< t << endl;
+    return 0;
 }
-int main(){	
-	char ch;
-	ifstream file("ProgramNotes\\CC-Assignment-File.txt");
-	string fileText="",word="";
-	while(file){
-		file.get(ch);
-		fileText += ch;
-	}
-	cout<<"-(TEXT INSIDE THE FILE)-\n"<<fileText<<endl;
-	cout<<"<-------------------------------------->\n";
-	cout<<"[Words]\t[Status]\n";
-	for(int i=0; fileText[i]!='\0'; i++){
-		if(fileText[i]!=' ')
-			word += fileText[i];
-		else{
-			cout<<"\""<<word<<"\""<<" : "<<checkWord(word)<<endl;	
-			word = "";
-		}
-	}
-	return 0;
+
+void read_from_file(){
+    ifstream fin("ProgramNotes//CC-Assignment-File.txt");
+    if(!fin){
+        cout << "Error opening file!" << endl;
+        exit(1);
+    }
+    fin.getline(buff, 1000); 
+    fin.close();
 }
+
+char lex_anal(){
+    int state = 1;
+    char ch;
+    while(1){
+        ch = next_char();
+        if(ch == '\0'){ 
+            if(state == 2)
+                return 'y'; 
+            else
+                return 'n'; 
+        }
+        if(ch == 'a' || ch == 'b'){
+            // toggle state
+            if(state == 1)
+                state = 2;
+            else
+                state = 1;
+        }
+        else{
+            cout << "Error\n";
+            exit(1);
+        }
+    }
+}
+
+char next_char(){
+    char c = buff[forward];
+    forward++;
+    return c;
+}
+
